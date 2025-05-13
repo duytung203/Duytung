0323d3bc1c3c8f59b60221e222558af6b8c6772b
DuytungTT <duytung2506@gmail.com>
2025-05-13 15:48:18 +0700

    hiệu ứng con trỏ chuột

--- scrip.js
+++ scrip.js
@@ -5,3 +5,11 @@ const btn = document.getElementById('helloBtn');
 {
     alert('Xin chào! Đây là thông báo từ JavaScript.');
 });
+
+
+const cursor = document.getElementById('cursor');
+
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = e.clientX + 'px';
+  cursor.style.top = e.clientY + 'px';
+});
